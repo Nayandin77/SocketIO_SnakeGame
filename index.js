0688903e31739c755ab889c04ea06deb5defff85
Dennis Nayandin <nayandindennis@yahoo.com>
2020-11-21 20:29:01 -0500

    created game State

--- index.js
+++ index.js
@@ -6,6 +6,29 @@ const gameScreen = document.getElementById('gameScreen');
 
 let canvas, ctx;
 
+const gameState = {
+    player: {
+        pos: {
+            x: 3,
+            y: 10,
+        },
+        vel: {
+            x: 1,
+            y: 0,
+        },
+        snake : [
+            {x: 1, y: 10},
+            {x: 2, y: 10},
+            {x: 3, y: 10},
+        ],
+    },
+    food: {
+        x: 7,
+        y: 7,
+    },
+    gridSize: 20,
+};
+
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
